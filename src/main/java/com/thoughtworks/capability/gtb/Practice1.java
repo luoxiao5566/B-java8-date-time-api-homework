379870fef02205543080b0df1170991af2dd896c
xiao luo <xiao.luo@thoughtworks.com>
2020-11-16 21:54:05 +0800

    [Luoxiao] feat: complete Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +14,17 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+
+        int year = date.getYear();
+        LocalDate laborDay = LocalDate.of(year, 5, 1);
+        long days = 0;
+        if (date.isBefore(laborDay)) {
+            days = date.until(laborDay, ChronoUnit.DAYS);
+        } else {
+            LocalDate nextLaborDay = laborDay.plusYears(1);
+            days = date.until(nextLaborDay, ChronoUnit.DAYS);
+        }
+        return days;
+    }
 }
